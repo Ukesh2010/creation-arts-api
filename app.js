154e5ee611daea1bb84ce61d01fed872f10b435a
umesh47 <071bel2014@gmail.com>
2021-04-17 19:48:57 +0545

    banner image api created

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-const bodyParser = require("body-parser");
-const passport = require("passport");
-var cors = require("cors");
+const bodyParser = require('body-parser')
+const passport = require('passport')
+var cors = require('cors')
 
-const users = require("./routes/api/users");
-const categories = require("./routes/api/categories");
-const products = require("./routes/api/products");
-const orders = require("./routes/api/orders");
+const users = require('./routes/api/users')
+const categories = require('./routes/api/categories')
+const products = require('./routes/api/products')
+const orders = require('./routes/api/orders')
+const setting = require('./routes/api/setting')
 
-app.use(cors());
-app.use("/static", express.static("uploads"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(passport.initialize());
+app.use(cors())
+app.use('/static', express.static('uploads'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(passport.initialize())
 
-require("./config/passport")(passport);
+require('./config/passport')(passport)
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to creation arts api" });
-});
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to creation arts api' })
+})
 
-app.use("/api/users", users);
-app.use("/api/categories", categories);
-app.use("/api/products", products);
-app.use("/api/orders", orders);
+app.use('/api/users', users)
+app.use('/api/categories', categories)
+app.use('/api/products', products)
+app.use('/api/orders', orders)
+app.use('/api/setting', setting)
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
 
-const mongoose = require("mongoose");
-const db = require("./config/keys").mongoURI;
+const mongoose = require('mongoose')
+const db = require('./config/keys').mongoURI
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch((err) => console.log(err));
+  .then(() => console.log('Connected to MongoDB successfully'))
+  .catch((err) => console.log(err))
---
+++ models/Setting.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+SettingSchema = new Schema({
+  bannerImage: [
+    {
+      originalname: { type: String, required: true },
+      mimetype: { type: String, required: true },
+      filename: { type: String, required: true },
+      path: { type: String, required: true },
+    },
+  ],
+})
+
+module.exports = Setting = mongoose.model('Setting', SettingSchema)
